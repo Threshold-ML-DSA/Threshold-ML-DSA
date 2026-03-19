bc4a9dc28fb1087b9db195357aaa3dd25c2c000a
manel1874 <manel1874@gmail.com>
2026-03-19 23:15:22 +0000

    fix: copy paste typos

--- implementation/sign/thmldsa/doc.go
+++ implementation/sign/thmldsa/doc.go
@@ -1,12 +1,7 @@
-// mldsa implements NIST post-quantum signature scheme ML-DSA (FIPS204)
+// Package thmldsa implements threshold signing for NIST ML-DSA (FIPS 204).
 //
-// Each of the three different security levels of ML-DSA is implemented by a
-// subpackage. For instance, mldsa44 can be found in
+// Each of the three parameter sets is implemented by a subpackage. For
+// instance, thmldsa44 can be found in
 //
-//	github.com/cloudflare/circl/sign/mldsa/mldsa44
-//
-// If your choice for mode is fixed compile-time, use the subpackages.
-// To choose a scheme at runtime, use the generic signatures API under
-//
-//	github.com/cloudflare/circl/sign/schemes
+//	https://github.com/Threshold-ML-DSA/Threshold-ML-DSA/tree/main/implementation/sign/thmldsa/thmldsa44
 package thmldsa
